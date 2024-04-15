72fb7bb1b310fc2e6aabcf1e3f64cb166b8dfc4c
tilakverma225@gmail.com <tilakverma225@gmail.com>
2024-04-15 19:42:17 +0530

    string code

---
+++ string_operations.py
@@ -0,0 +1,13 @@
+name ="tialk "
+sirname="verma"
+length=len(name)
+print("length of string ",length)
+
+fullname= name + sirname
+print("my full name is",fullname)
+
+print(name.endswith("alk"))
+print(name.capitalize())
+print(name.replace("a","1"))
+print(name.find("v"))
+print(name.count("a"))
