e25538768a276b45c09db2a3147c81e6f1e9f5f8
tilakverma225@gmail.com <tilakverma225@gmail.com>
2024-04-15 22:24:47 +0530

    greatest number

---
+++ find_greatest_number.py
@@ -0,0 +1,10 @@
+a=int(input("enter a :"))
+b=int(input("enter b :"))
+c=int(input("enter c :"))
+
+if(a>b and a>c):
+  print("a is greatest number")
+elif(b>c):
+  print("b is greatest number")
+else:
+  print("c is grestest number ")
